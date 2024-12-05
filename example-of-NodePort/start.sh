e98ab130224b6e937bec3cc392811d928dec33f2
David North <bandanna2k@gmail.com>
2024-12-06 11:07:41 +1300

    K8S Education - Fix up NodePort example

--- example-of-NodePort/start.sh
+++ example-of-NodePort/start.sh
@@ -7,7 +7,12 @@ minikube start
 minikube kubectl -- wait pod --all --for=condition=Ready --namespace=kube-system --timeout=60s
 
 source apply.sh
+minikube kubectl -- wait pod --all --for=condition=Ready --namespace=education --timeout=60s
 
-# Monitor
-watch minikube kubectl -- get pods -A
+URL1=`minikube service -n education node-port1 --url`
+URL2=`minikube service -n education node-port2 --url`
+
+set -x
+curl -I $URL1
+curl -I $URL2
 
