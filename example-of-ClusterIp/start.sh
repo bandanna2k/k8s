33512d653dd6ec7065dc5ca8cdfdff40aa74436e
David North <bandanna2k@gmail.com>
2024-12-06 11:38:17 +1300

    K8S Education - ClusterIp example WIP

---
+++ example-of-ClusterIp/start.sh
@@ -0,0 +1,17 @@
+set -e
+
+source stop.sh
+source lint.sh
+
+# Start and wait to be ready
+minikube start
+minikube kubectl -- wait pod --all --for=condition=Ready --namespace=kube-system --timeout=60s
+
+source apply.sh
+minikube kubectl -- wait pod --all --for=condition=Ready --namespace=education --timeout=60s
+
+set -x
+minikube kubectl -- proxy --port=8080 &
+sleep 1
+
+curl -I http://localhost:8080/api/v1/namespaces/education/services/cluster-ip1/proxy
